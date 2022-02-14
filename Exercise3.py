030f0bb8f9ebf59b75d8d2694bc044fb992db03a
KJDolch <K.J.Dolch@web.de>
2022-02-14 15:39:28 +0100

    3added

---
+++ Exercise3.py
@@ -0,0 +1,53 @@
+"""
+Exercise 3 - Math operations
+"""
+
+nr1 = 2
+nr2 = 4
+nr3 = 5
+nr4 = 7.5
+nr5 = 10
+
+
+sum = nr1 + nr2 + nr3 + nr5
+print(sum)
+
+substraction = nr5 - 3
+print(substraction)
+
+multiplication = nr1 * nr4
+print(multiplication)
+print(type(multiplication))
+
+division = nr5 / nr3
+print(division)
+
+IntegerDivision = nr4 // 2
+print(IntegerDivision)
+
+Power = 4 ** 5
+print(Power)
+
+Remainder = nr5 % nr4
+print(Remainder)
+
+nr1 += 20
+print(nr1)
+
+nr2 -= 5
+print(nr2)
+
+nr3 *= 3
+print(nr3)
+
+nr4 /= 2
+print(nr4)
+
+nr5 //= 3
+print(nr5)
+
+nr1 **= 2
+print(nr1)
+
+nr1 %= 3
+print(nr1)
