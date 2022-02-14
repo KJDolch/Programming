d9ded256d87f2587d0907266c1ffe9615596cf0a
KJDolch <K.J.Dolch@web.de>
2022-02-14 10:00:21 +0100

    7

---
+++ Exercise7.py
@@ -0,0 +1,32 @@
+"""
+Exercise 7 - Other comparisons
+"""
+
+Nr1 = int(input("Give me one number: "))
+Nr2 = int(input("Give me another one: "))
+Nr3 = int(input("and one more: "))
+Nr4 = int(input("and a last one: "))
+
+if Nr1 > Nr2 and Nr3 > Nr4:
+    print("You are great!")
+
+elif Nr1 < Nr3 and Nr2 < Nr4:
+    print("You are great!!")
+
+elif Nr1 == Nr4 or Nr2 >= Nr3:
+    print("You are great!!!")
+
+elif Nr1 <= Nr4 and Nr2 == Nr3:
+    print("You are great!!!!")
+
+else:
+    print("Nope")
+
+if Nr1 == Nr2 == Nr3 == Nr4:
+    print("Jackpot")
+
+elif Nr1 != Nr2 and Nr1 != Nr3 and Nr1 != Nr4:
+    print("Looser")
+
+else:
+    print("as always")
